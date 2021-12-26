ea6495035d58afe40b650a41cde243af226b8d87
Carson <rodriguescarson@hotmail.com>
2021-12-26 19:22:29 +0530

    add: controller for login and onboard

--- JavaFXApplication.java
+++ JavaFXApplication.java
@@ -23,7 +23,7 @@ public class JavaFXApplication extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("Home.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
         
         Scene scene = new Scene(root);
         
